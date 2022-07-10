4cfa9e0a2abfb0011e93b712fe5bae7e4be1991f
Bertoldijulian <julianbertoldi12@hotmail.com.ar>
2022-07-10 13:46:29 -0300

    NavBar
    creacion NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h1>Elegance</h1>
+        <ul>
+            <li><a href="#">Perfumes</a></li>
+            <li><a href="#">Relojes</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
